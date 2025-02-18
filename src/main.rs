5408b71ab89c6a2498f5a34625a7fa62524e7134
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-02-18 16:02:05 +0100

    document base

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #[allow(non_snake_case)]
-
-pub mod ui;
+pub mod base;
+/// # Implementation of the model
+/// the implementation of the model and all of the structs used to model parts of the simulation.
+/// It also includes the view functions for the egui struct edit view.
 pub mod model;
 pub mod regressor;
-pub mod base;
+pub mod ui;
 
 use eframe::egui;
 use ui::{app::BionApp, Front};
 
-
-
+/// Main function that initializes logging and sets up the Eframe app
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
@@ -20,15 +21,13 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "My egui App",
         options,
-        Box::new(|_cc| {
-            Box::<BionApp>::default()
-        }),
+        Box::new(|_cc| Box::<BionApp>::default()),
     )
 }
 
 impl eframe::App for BionApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::SidePanel::left("options").show(ctx, |ui| { self.left_panel(ui, ctx)});     
-        egui::CentralPanel::default().show( ctx, |ui|{ self.center_panel(ui, ctx) });
+        egui::SidePanel::left("options").show(ctx, |ui| self.left_panel(ui, ctx));
+        egui::CentralPanel::default().show(ctx, |ui| self.center_panel(ui, ctx));
     }
 }
